7f9968dad608f51321c4d0a8274bcd7987689796
Brian King <defectivelogic@gmail.com>
2013-07-12 04:37:36 +0000

    Modify web.js to read welcome message from index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    welcomeMsg = fs.readFileSync('index.html');
+    response.send(welcomeMsg.toString());
 });
 
 var port = process.env.PORT || 5000;
