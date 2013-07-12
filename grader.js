cc051c1c08251c533d1e76fa939400bf0de1ef43
Brian King <defectivelogic@gmail.com>
2013-07-12 06:34:41 +0000

    Extend grader.js to use restler and accept --url option.

--- grader.js
+++ grader.js
@@ -21,11 +21,13 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
+var rest = require('restler');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = 'http://obscure-plains-1069.herokuapp.com';
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,14 +63,36 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var checkFromUrl = function(url, checksfile) {
+    rest.get(url).on('complete', function(data) {
+	$ = cheerio.load(data);
+	var checks = loadChecks(checksfile).sort();
+	var out = {};
+	for (var k in checks) {
+		var present = $(checks[k]).length > 0;
+		out[checks[k]] = present;
+	}
+	var outJson = JSON.stringify(out, null, 4);
+	console.log(outJson);
+    });
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', '') // , clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url to check', URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+
+    if (program.file.length > 0) {
+    	var checkResults = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkResults, null, 4);
+    	console.log(outJson);
+    } else if (program.url.length > 0) {
+	checkFromUrl(program.url, program.checks);
+    }
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
